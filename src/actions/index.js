028de23310ee2bbbd2695aa23d2477b194796c95
Ajay David <antonio.g.david@gmail.com>
2020-07-29 18:25:17 -0400

    dispatch action after stream creation

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import streams from "../apis/streams";
-import { SIGN_IN, SIGN_OUT } from "./types";
+import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from "./types";
 
 export const signIn = (userId) => {
   return {
@@ -19,6 +19,8 @@ export const createStream = (formValues) => {
   // utilize redux-thunk
   return async (dispatch) => {
     // POST /streams endpoint
-    streams.post("/streams", formValues);
+    const response = await streams.post("/streams", formValues);
+
+    dispatch({ type: CREATE_STREAM, payload: response.data });
   };
 };
